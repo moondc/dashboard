04a765cf8a1ac012bce31a45a7b982bfb1dc607d
Daniel Moon <daniel_moon16@yahoo.com>
2024-08-01 10:26:34 -0400

    added side-nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SideNavComponent, MatSlideToggleModule],
+  imports: [RouterOutlet, SideNavComponent, MatSlideToggleModule, MatButtonModule, MatDividerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-side-nav',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './side-nav.component.html',
   styleUrl: './side-nav.component.scss'
 })
